d6ba97371cd95d1c2a1ea279cb62dfd167405bbe
Andrew Scholz <yoitsscholzy@gmail.com>
2019-03-11 14:25:20 -0400

    navbar complete

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import React from "react"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h3>Hello from Gatsby!</h3>
   </Layout>
 )
 
